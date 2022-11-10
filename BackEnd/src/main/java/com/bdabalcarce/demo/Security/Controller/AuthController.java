a623fecba8e231f2f7b5f0cd32a2fd5b1accf1f2
Federico Di Cillo <99351705+federicoDicillo@users.noreply.github.com>
2022-11-09 22:12:26 -0300

    Update AuthController.java

--- BackEnd/src/main/java/com/bdabalcarce/demo/Security/Controller/AuthController.java
+++ BackEnd/src/main/java/com/bdabalcarce/demo/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://login-bda-balcarce.netlify.app")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping({"/auth"})
 public class AuthController {
 
